348e633969066fa7abb2574bda14446b9deb8c72
yourname <youremail@address.com>
2017-03-14 19:08:32 +0200

    up

--- my-app/src/app/nav-menu/nav-menu.component.ts
+++ my-app/src/app/nav-menu/nav-menu.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class NavMenuComponent implements OnInit {
 
   myList = [
-    {id:1, txt: 'Button1'},
-    {id:2, txt: 'Button2'},
-    {id:3, txt: 'Button3'},
-    {id:4, txt: 'Button4'},
+    {id:1, txt: "Button1"},
+    {id:2, txt: "Button2"},
+    {id:3, txt: "Button3"},
+    {id:4, txt: "Button4"},
   ]
   constructor() { }
 
